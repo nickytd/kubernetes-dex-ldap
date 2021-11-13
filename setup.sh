2a1c73aabb47a5fad413650a877ad83491eff6e0
Niki Dokovski <nikolai.dokovski@sap.com>
2021-11-13 10:44:09 +0100

    using cert manager

--- setup.sh
+++ setup.sh
@@ -7,19 +7,9 @@ source $dir/.includes.sh
 
 check_executables
 
-
 kubectl apply -k $dir
 
-if [ -d $dir/dex/ssl ]; then
-  certs=("dex")
-  for c in ${certs[@]}; do
-    kubectl create secret tls "$c-tls" -n dex \
-      --cert=$dir/dex/ssl/tcl.crt \
-      --key=$dir/dex/ssl/tcl.key \
-      --dry-run=client -o yaml | kubectl apply -f - 
-  done 
+kubectl apply -f $dir/dex/ingress.yaml \
+  --dry-run=client -o yaml | kubectl apply -f -
 
-  kubectl apply -f $dir/dex/ingress.yaml \
-      --dry-run=client -o yaml | kubectl apply -f -
-fi
 
